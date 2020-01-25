15342a8634c2022aa807af98fda17f73be656375
ropi <37555078+r-o-p-i@users.noreply.github.com>
2020-01-26 00:38:46 +0300

    Новогодний подарок

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,9 @@ public class Main {
     public static void main(String[] args) {
         NewYearGift ng = new NewYearGift("Новогодний подарок №1 ");
         System.out.println(ng.toString());
-        //  NewYearGift ng1 = new NewYearGift("Новогодний подарок №2 ");
-        // System.out.println(ng1.toString());
-        //  Sladost sl= Tomtom.Create();
+        NewYearGift ng1 = new NewYearGift("Новогодний подарок №2 ");
+        System.out.println(ng1.toString());
 
-        //  System.out.println(sl.toString());
+
     }
 }
